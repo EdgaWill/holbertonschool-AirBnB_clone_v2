1dd097e0d22ae64c8dcf8be36e1b14dbc08f369a
edgawill <edgar-william@hotmail.com>
2023-03-07 04:41:31 +0000

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 ''' Start a Flask web '''
-
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def greeting():
-        ''' Display a message to user '''
-            return 'Hello HBNB!'
+    ''' Display a message to the user '''
+    return 'Hello HBNB!'
 
 
-        if __name__ == '__main__':
-                app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
