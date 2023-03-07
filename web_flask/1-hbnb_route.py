ca2cd8753c73c9c14e0f7399d69e8f56aeb0341f
edgawill <edgar-william@hotmail.com>
2023-03-07 04:38:36 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Start a Flask web '''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greeting():
+    ''' Display a message to the user '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def greeting_hbnb():
+    ''' Display a 'HBNB' to the user '''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
